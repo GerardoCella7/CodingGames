058dabdda0bdaf841c3c5131692bddb2f128945e
Gerardo Cella <66060626+GerardoCella7@users.noreply.github.com>
2021-06-13 03:17:36 +0200

    v2

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,72 +1,46 @@
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-
-const Jeu = class{
+const Game = class{
     constructor(){
-        this.plateau = [];
-        this.coup = [];
+        this.grid = [];
+        this.pion = {
+            player: 'O',
+            bot: 'X',
+            empty: '.'
+        };
     }
 
-    setPlateau(ligne, data){
-        this.plateau[ligne] = [...data];
-        this.coup[ligne] = [...data];
+    setGrid(data){
+        this.grid.push(...data);
     }
 
-    getCoup(){
-        this.getProchainCoup();
-
-        for (let i = 0; i < 3; i++) {
-            this.coup[i] = this.coup[i].join('');
-            this.plateau[i] = this.plateau[i].join('');
-        }
-        const sortie = this.coup.join('\n');
-
-        return sortie != this.plateau.join('\n') ? sortie : 'false';
-    }
-
-    getProchainCoup(){
-        for (let i = 0; i < this.coup.length; i++) {
-            for (let j = 0; j < this.coup[i].length; j++) {
-                const tmp = [[...this.coup[0]], [...this.coup[1]], [...this.coup[2]]];
-                if(this.coup[i][j] == '.'){
-                    tmp[i][j] = 'O';
-                    if(this.coupGagnant(i, j, tmp)){
-                        this.coup[i][j] = 'O';
-                        return;
-                    }
+    getMove(){
+        let playMove = null;
+        ['123','456','789','147','258','369','159','357'].forEach(e => {
+            let nbPlayerPion = 0;
+            let positionEmpty = null;
+            e.split('').forEach(i => {
+                nbPlayerPion += this.grid[i-1] === this.pion.player ? 1 : 0;
+                positionEmpty = this.grid[i-1] === this.pion.empty ? i-1 : positionEmpty;
+            });
+            if(nbPlayerPion === 2 && positionEmpty != null){
+                // win
+                const move = [...this.grid];
+                move[positionEmpty] = this.pion.player;
+                const nextMoveStr = [];
+                for(let i = 0; i < 3; i++){
+                    const str = move[i*3] + move[i*3+1] + move[i*3+2];
+                    nextMoveStr.push(str);
                 }
+                playMove = nextMoveStr.join('\n');
             }
-        }
-    }
-
-    coupGagnant(i, j, tmp = null){
-        function horizontal(){
-            const l = tmp[i];
-            return l.join('') == 'OOO';
-        }
-        function vertical(){
-            const l = [tmp[0][j], tmp[1][j], tmp[2][j]];
-            return l.join('') == 'OOO';
-        }
-        function diagonal(){
-            const l1 = [tmp[0][0], tmp[1][1], tmp[2][2]];
-            const l2 = [tmp[0][2], tmp[1][1], tmp[2][0]];
-            return l1.join('') == 'OOO' || l2.join('') == 'OOO';
-        }
-
-        return horizontal() || vertical() || diagonal();
+        });
+        return playMove === null ? 'false' : playMove;
     }
 }
-const jeu = new Jeu();
 
+const game = new Game();
 
 for (let i = 0; i < 3; i++) {
-    jeu.setPlateau(i, readline().split(''));
+    game.setGrid(readline().split(''));
 }
 
-// Write an answer using console.log()
-// To debug: console.error('Debug messages...');
-
-console.log(jeu.getCoup());
+console.log(game.getMove());
